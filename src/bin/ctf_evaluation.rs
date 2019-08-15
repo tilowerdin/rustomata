72a886530cf00a86aff91d93dca54282d3e379f4
Tilo Werdin <tilo.werdin@gmail.com>
2019-08-15 12:22:50 +0200

    removed weights from reverse_transition_map for finding the transitions with accumulated weights

--- src/bin/ctf_evaluation.rs
+++ src/bin/ctf_evaluation.rs
@@ -3,7 +3,17 @@
 #![allow(unused_variables)]
 
 use clap::{App, ArgMatches, SubCommand, Arg};
-
+use rustomata::automata::tree_stack_automaton::TreeStackAutomaton;
+use rustomata::grammars::pmcfg::{PMCFG,PMCFGRule};
+use log_domain::LogDomain;
+use rustomata::approximation::tts::TTSElement;
+use rustomata::approximation::ApproximationStrategy;
+use rustomata::approximation::equivalence_classes::EquivalenceRelation;
+use rustomata::approximation::relabel::RlbElement;
+use rustomata::automata::tree_stack_automaton::PosState;
+use rustomata::recognisable::Recognisable;
+use rustomata::recognisable::Item;
+use rustomata::recognisable::automaton::Automaton;
 
 
 const GRAMMAR_STRING : &str = "
@@ -57,6 +67,90 @@ pub fn handle_sub_matches(ctf_matches: &ArgMatches) {
 }
 
 pub fn test() {
+    let accepting_input = vec!["a".to_string(), "b".to_string(), "c".to_string(), "d".to_string()];
 
+    let g : PMCFG<String,String,LogDomain<f64>> = GRAMMAR_STRING.parse().unwrap();
+
+    let a = TreeStackAutomaton::from(g);
+
+    println!("------ automaton a ------");
+    println!("{}", &a);
+    println!();
+    println!();
+
+    let tts = TTSElement::new();
+
+    let (b,strat1) = tts.approximate_automaton(&a);
+
+    println!("------ automaton b ------");
+    println!("{}", &b);
+    println!();
+    println!();
+
+    let e: EquivalenceRelation<PMCFGRule<_,_,_>, String> = CLASSES_STRING.parse().unwrap();
+    let f = |ps: &PosState<_>| ps.map(|nt| e.project(nt));
+    let rlb = RlbElement::new(&f);
+
+    let (c,strat2) = rlb.approximate_automaton(&b);
+
+    println!("------ automaton c ------");
+    println!("{}", &c);
+    println!();
+    println!();
+
+    // for trans in c.transitions() {
+    //     println!("------ unapproximate {} ------", &trans);
+    //     for unapprox in strat2.unapproximate_transition(&trans) {
+    //         println!("{}", unapprox);
+    //     }
+    //     println!();
+    //     println!();
+    // }
+
+
+
+    let recs_c = c.recognise(accepting_input.clone());
+
+    for Item(_,run_c) in recs_c {
+        println!("------ run c ------");
+        println!("{:?}", &run_c);
+        println!();
+        println!();
+
+        let unapproxs2 = strat2.unapproximate_run(run_c);
+        for unapprox2 in unapproxs2 {
+            // println!("------ unapproximated run ------");
+            // println!("{:?}", &unapprox2);
+            // println!();
+            // println!();
+            let checked_bs = b.check_run(unapprox2);
+            for Item(_,checked_b) in checked_bs {
+                println!("------ run b ------");
+                println!("{:?}", &checked_b);
+                println!();
+                println!();
+
+                let unapproxs1 = strat1.unapproximate_run(checked_b);
+                for unapprox1 in unapproxs1 {
+                    let checked_as = a.check_run(unapprox1);
+                    for Item(_,checked_a) in checked_as {
+                        println!("------ run a ------");
+                        println!("{:?}", &checked_a);
+                        println!();
+                        println!();
+                    }
+                }
+            }
+        }
+    }
+
+    let runs = a.recognise(accepting_input.clone());
+    for Item(_,run) in runs {
+        println!("------ orig run a ------");
+        println!("{:?}", &run);
+        println!();
+        println!();
+    }
+    
 }
 
