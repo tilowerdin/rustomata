5731e304fa61bd1e774395ca920ca0e2f57fcecf
Tilo Werdin <tilo.werdin@gmail.com>
2019-08-20 12:10:00 +0200

    added test for testing the collection of similar transitions during the approximation

--- tests/approximation_tests.rs
+++ tests/approximation_tests.rs
@@ -6,13 +6,12 @@ use rustomata::grammars::pmcfg::PMCFGRule;
 use rustomata::approximation::equivalence_classes::EquivalenceRelation;
 use log_domain::LogDomain;
 use rustomata::grammars::pmcfg::PMCFG;
-use rustomata::approximation::relabel::RlbElement;
+use rustomata::approximation::relabel::{RlbElement,RlbElementTSA};
 use rustomata::approximation::tts::TTSElement;
 use rustomata::approximation::ApproximationStrategy;
 use rustomata::recognisable::Recognisable;
 
-#[test]
-fn test_unapproximate_runs() {
+fn get_grammar_string() -> String {
     // creating the grammar string that accepts a^i b^j c^i d^j
     let mut grammar_string = String::new();
     grammar_string.push_str("initial: [S]\n");
@@ -22,16 +21,32 @@ fn test_unapproximate_runs() {
     grammar_string.push_str("A → [[],  []                             ] (    )   # 0.5\n");
     grammar_string.push_str("B → [[T b, Var 0 0],  [T d, Var 0 1]     ] (B   )   # 0.5\n");
     grammar_string.push_str("B → [[],  []                             ] (    )   # 0.5\n");
+    grammar_string
+}
 
+fn get_classes_string() -> String {
     // creating the equivalence classes from PMCFGRule to String
     let mut classes_string = String::new();
     classes_string.push_str("S [\"S → [[Var 0 0, Var 1 0, Var 0 1, Var 1 1]] (A, B)\"]\n");
     classes_string.push_str("A [\"A → [[T a, Var 0 0],  [T c, Var 0 1]     ] (A   )   # 0.5\", \"A → [[],  []] (    )   # 0.5\"]\n");
     classes_string.push_str("B [\"B → [[T b, Var 0 0],  [T d, Var 0 1]     ] (B   )   # 0.5\", \"B → [[],  []] (    )   # 0.5\"]\n");
     classes_string.push_str("R *\n");
+    classes_string
+}
+
+fn get_accepting_input() -> Vec<String> {
+    vec!["a".to_string(), "b".to_string(), "c".to_string(), "d".to_string()]
+}
+
+#[test]
+fn test_unapproximate_runs() {
+    
+    let grammar_string = get_grammar_string();
+    let classes_string = get_classes_string();
+    
 
     // creating an accepting input vector
-    let accepting_input = vec!["a".to_string(), "b".to_string(), "c".to_string(), "d".to_string()];
+    let accepting_input = get_accepting_input();
 
     // parse the grammar into a PMCFG
     let g : PMCFG<String,String,LogDomain<f64>> = grammar_string.parse().unwrap();
@@ -77,4 +92,31 @@ fn test_unapproximate_runs() {
 
     // unapproximating the runs should result in one accepting run of the original TSA since the input is an accepting one
     assert!(got_valid_run);
+}
+
+#[test]
+fn test_collecting_weights_of_similar_transitions() {
+    let grammar_string = get_grammar_string();
+    let classes_string = get_classes_string();
+    let accepting_input = get_accepting_input();
+
+    let g : PMCFG<String, String,LogDomain<f64>> = grammar_string.parse().unwrap();
+
+    let a = TreeStackAutomaton::from(g);
+
+    let e: EquivalenceRelation<PMCFGRule<_,_,_>, String> = classes_string.parse().unwrap();
+    let f = |ps: &PosState<_>| ps.map(|nt| e.project(nt));
+    let rlb = RlbElementTSA::new(&f);
+
+    let (b,_) = rlb.approximate_automaton(&a);
+
+    let recs = b.recognise(accepting_input);
+
+    let mut count = 0;
+
+    for _ in recs {
+        count += 1;
+    }
+
+    assert_eq!(1, count);
 }
